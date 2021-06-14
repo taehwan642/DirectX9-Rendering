1e78943b311a195996af73532184a0f75530f692
Taehwan Kim <49132911+taehwan642@users.noreply.github.com>
2021-06-14 21:04:33 +0900

    Updated XFile

--- Code/7.XFile.cpp
+++ Code/7.XFile.cpp
@@ -31,12 +31,43 @@ namespace XFile
 		if (FAILED(D3DXCreateTextureFromFile(device, finalpath, &texture)))
 			return E_FAIL;
 
+		device->SetRenderState(D3DRS_DITHERENABLE, FALSE);
+		device->SetRenderState(D3DRS_SPECULARENABLE, FALSE);
+		device->SetRenderState(D3DRS_ZENABLE, TRUE);
+		device->SetRenderState(D3DRS_AMBIENT, 0x000F0F0F);
+		device->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+		device->SetRenderState(D3DRS_LIGHTING, FALSE);
+
+		device->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_SELECTARG1);
+		device->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+
+		// D3DTEXF_LINEAR == Bilinear Interpolation
+		// D3DTEXF_POINT == Nearest neighbor Interpolation
+		device->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+		device->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+
 		return S_OK;
 	}
 	
 	void SetMatrices(LPDIRECT3DDEVICE9 device)
 	{
+		D3DXMATRIX mWorld, mView, mProj;
+		D3DXMatrixIdentity(&mWorld);
+		device->SetTransform(D3DTS_WORLD, &mWorld);
+		
+		D3DXVECTOR3 eye{ 0.f,5.f,-8.f };
+		D3DXVECTOR3 lookat{ 0.f,-2.f,0.f };
+		D3DXVECTOR3 up{ 0.f,1.f,0.f };
+		D3DXMatrixLookAtLH(&mView, &eye, &lookat, &up);
+		device->SetTransform(D3DTS_VIEW, &mView);
 
+		D3DXMatrixPerspectiveFovLH(&mProj, 
+			D3DX_PI / 4, // 시야각
+			1280 / 720,  // 화면 비율
+			1.f,		 // 전방 클립면
+			100.f);		 // 후방 클립면
+
+		device->SetTransform(D3DTS_PROJECTION, &mProj);
 	}
 
 	void Update(LPDIRECT3DDEVICE9 device)
